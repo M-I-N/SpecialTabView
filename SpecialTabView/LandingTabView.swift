3d3b4e7ad4b0cf7610caae0dc32a00955fc3256f
Mufakkharul Islam Nayem <mufakkharul.nayem@bjitgroup.com>
2020-01-01 14:52:53 +0600

    Revert back to previously selected tab when sheet is dismissed.

--- SpecialTabView/LandingTabView.swift
+++ SpecialTabView/LandingTabView.swift
@@ -9,41 +9,65 @@
 import SwiftUI
 
 struct LandingTabView: View {
-
+    @EnvironmentObject var appState: AppState
     @State var showAddSheet: Bool = false
 
     var body: some View {
-        TabView {
+
+        TabView(selection: $appState.currentTabSelection) {
 
             Text("List View")
                 .tabItem {
                     Image(systemName: "list.bullet.below.rectangle")
             }
+            .tag(0)
 
             SheetPresenter(presentingSheet: self.$showAddSheet, content: Text("Add New"))
                 .tabItem {
                     Image(systemName: "plus.circle.fill")
             }
+            .tag(1)
 
             Text("Grid View")
                 .tabItem {
                     Image(systemName: "square.grid.2x2")
             }
+            .tag(2)
+
+            Text("Options View")
+                .tabItem {
+                    Image(systemName: "ellipsis")
+            }
+            .tag(3)
+
         }
     }
 }
 
 struct SheetPresenter<Content>: View where Content: View {
+    @EnvironmentObject var appState: AppState
     @Binding var presentingSheet: Bool
+
     var content: Content
     var body: some View {
         Rectangle().fill(Color.clear)
-            .sheet(isPresented: self.$presentingSheet, content: { self.content })
+            .sheet(isPresented: self.$presentingSheet, onDismiss: {
+                self.appState.currentTabSelection = self.appState.previousTabSelection
+            }) { self.content }
             .onAppear {
                 DispatchQueue.main.async {
                     self.presentingSheet = true
                 }
             }
+    }
+}
+
+class AppState: ObservableObject {
+    private (set) var previousTabSelection = -1
+    @Published var currentTabSelection = 0 {
+        didSet {
+            previousTabSelection = oldValue
+        }
     }
 }
 
